247ecc889c100ffe9ddce0c6c8fa21ec3d4e80f9
Harish Kilaru <hckilaru@gmail.com>
2016-05-28 03:11:56 -0400

    finished stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,39 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return ('' + obj + '');
+  }
+  else if(typeof obj === 'string') {
+    return ('"' + obj + '"');
+  }
+  else if(Array.isArray(obj)) {
+    var objArrVals = [];
+    obj.forEach(function(element) {
+      objArrVals.push(stringifyJSON(element));
+    });
+    return '[' + objArrVals + ']';
+  }
+  else if(typeof obj === "object") {
+    var objKeys = [];
+    objKeys = Object.keys(obj);
+    var objKeyVals = [];
+    objKeys.forEach(function(key) {
+      var keyString = '"' + key + '":';
+      var val = obj[key];
+      if(typeof val === "function" || typeof val === undefined) {
+        objKeyVals.push('');
+      }
+      else if(typeof val === "string") {
+        objKeyVals.push(keyString + '"' + val + '"');
+      }
+      else if(typeof val === "boolean" || typeof val === "number" || val === null) {
+        objKeyVals.push(keyString + val);
+      }
+      else if(typeof val === "object") {
+        objKeyVals.push(keyString + stringifyJSON(val));
+      }
+      });
+      return '{' + objKeyVals + '}';
+  }
 };
